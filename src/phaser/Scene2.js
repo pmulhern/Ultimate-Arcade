b11cd97f44c578fa9bfce02d1f2adc5c84459b99
Bobby Jones Jr <jones9682@yahoo.com>
2020-10-15 22:44:40 -0400

    added animation physics for power-up

--- src/phaser/Scene2.js
+++ src/phaser/Scene2.js
@@ -52,6 +52,53 @@ class Scene2 extends Phaser.Scene {
     this.input.on('gameobjectdown', this.destroyShip, this);
 
     this.add.text(20, 20, "Playing game", {font: "25px Areial", fill: "yellow"});
+
+    this.anims.create({
+      key: "red",
+      frames: this.anims.generateFrameNumbers("power-up", {
+        start: 0,
+        end: 1
+      }),
+      frameRate: 20,
+      repeat: -1
+    });
+    this.anims.create({
+      key: "gray",
+      frames: this.anims.generateFrameNumbers("power-up", {
+        start: 2,
+        end: 3
+      }),
+      frameRate: 20,
+      repeat: -1
+    });
+
+    this.physics.world.setBoundsCollision();
+
+    this.powerUps = this.physics.add.group();
+
+    // Add multiple objects
+    var maxObjects = 4;
+    for (var i = 0; i <= maxObjects; i++) {
+      var powerUp = this.physics.add.sprite(16, 16, "power-up");
+      this.powerUps.add(powerUp);
+       powerUp.setRandomPosition(0, 0, this.game.config.width, this.game.config.height);
+
+      // set random animation
+      if (Math.random() > 0.5) {
+        powerUp.play("red");
+      } else {
+        powerUp.play("gray");
+      }
+
+      // setVelocity
+      powerUp.setVelocity(100, 100);
+      // set boundaries
+      powerUp.setCollideWorldBounds(true);
+      // make power-ups bounce around screen
+     powerUp.setBounce(1);
+
+    }
+
   }
 
   update() {
