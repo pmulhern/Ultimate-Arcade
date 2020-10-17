613256d88aba744d8f97237120a33ce6d1f16a9f
Kevin <millhousekevin@gmail.com>
2020-10-17 17:07:41 -0400

    center that jawn you needed centered

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -5,6 +5,7 @@ export default class App extends React.Component {
 		return (
 			<div style={{ textAlign: "center" }}>
 				<h1>Galactic Attack!!</h1>
+				<div id="phaser"></div>
 			</div>
 		);
 	}
