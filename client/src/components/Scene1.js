87f0adebb9118897f8f99ee6749d05f910bc730c
Bobby Jones Jr <jones9682@yahoo.com>
2020-10-17 16:58:45 -0400

    Beam (#5)
    * added beam sprite
    
    * added class beam and constructor scene
    
    * added beam to preload and create animation
    
    * shifited code around and added beam functionality

--- client/src/components/Scene1.js
+++ client/src/components/Scene1.js
@@ -30,6 +30,10 @@ class Scene1 extends Phaser.Scene {
       frameWidth: 16,
       frameHeight: 24
     });
+    this.load.spritesheet("beam", "/src/assets/spritesheets/beam.png",{
+      frameWidth: 16,
+      frameHeight: 16
+    });
   }
   create() {
     this.add.text(20, 20, "Loading game...");
@@ -81,6 +85,12 @@ class Scene1 extends Phaser.Scene {
     this.anims.create({
       key: "thrust",
       frames: this.anims.generateFrameNumbers("player"),
+      frameRate: 20,
+      repeat: -1
+    });
+    this.anims.create({
+      key: "beam_anim",
+      frames: this.anims.generateFrameNumbers("beam"),
       frameRate: 20,
       repeat: -1
     });
