174d782db3bcf9077796f5132a005a09d02c9c37
Bobby Jones Jr <jones9682@yahoo.com>
2020-10-17 22:27:32 -0400

    added logic that will hurt player when collide with enemy and destroy enemy with beam projectiles

--- client/src/components/Scene2.js
+++ client/src/components/Scene2.js
@@ -17,6 +17,11 @@ class Scene2 extends Phaser.Scene {
     this.ship1 = this.add.sprite(this.game.config.width / 2 - 50, this.game.config.height / 2, "ship");
     this.ship2 = this.add.sprite(this.game.config.width / 2, this.game.config.height / 2, "ship2");
     this.ship3 = this.add.sprite(this.game.config.width / 2 + 50, this.game.config.height / 2, "ship3");
+
+    this.enemies = this.physics.add.group();
+    this.enemies.add(this.ship1);
+    this.enemies.add(this.ship2);
+    this.enemies.add(this.ship3);
     
     this.player = this.physics.add.sprite(this.game.config.width / 2 - 8, this.game.config.height - 64, "player");
     this.player.play("thrust");
@@ -73,16 +78,33 @@ class Scene2 extends Phaser.Scene {
       // make power-ups bounce around screen
       powerUp.setBounce(1);
     }
+
+    /* this will make projectiles move the powerups when they collide*/
     this.physics.add.collider(this.projectiles, this.powerUps, function(projectile, powerUp) {
       projectile.destroy();
     });
 
     this.physics.add.overlap(this.player, this.powerUps, this.pickPowerUp, null, this);
+    this.physics.add.overlap(this.player, this.enemies, this.hurtPlayer, null, this);
+    this.physics.add.overlap(this.projectiles, this.enemies, this.hitEnemy, null, this);
 
   }
-
+  /* will be able to pickup the powerups*/
   pickPowerUp(player, powerUp) {
     powerUp.disableBody(true, true);
+  }
+
+  /* will hurt player when collide with enemy*/
+  hurtPlayer(player, enemy) {
+    this.resetShipPos(enemy);
+    player.x = config.width / 2 -8;
+    player.y = config.height - 64;
+  }
+
+  /* projectiles will destroy ship*/
+  hitEnemy(projectile, enemy) {
+    projectile.destroy();
+    this.resetShipPos(enemy);
   }
 
   update() {
