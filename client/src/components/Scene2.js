c73d3b3478441661ef909b0325b3eb8897ee40ba
Bobby Jones Jr <jones9682@yahoo.com>
2020-10-20 14:56:05 -0400

    changed loop musicConfig to true

--- client/src/components/Scene2.js
+++ client/src/components/Scene2.js
@@ -108,7 +108,7 @@ class Scene2 extends Phaser.Scene {
       rate: 1,
       detune: 0,
       seek: 0,
-      loop: false,
+      loop: true,
       delay: 0
     }
     this.music.play(musicConfig);
