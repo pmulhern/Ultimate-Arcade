21d5f9d446bbfcdf403facb226f9cedb849792e9
Kevin <millhousekevin@gmail.com>
2020-10-25 11:15:44 -0400

    deploy

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 // import * as serviceWorker from './serviceWorker';
-import AuthProvider from './Context/AuthContext';
+import AuthProvider from './context/AuthContext';
 
 ReactDOM.render(<AuthProvider> <App /> </AuthProvider>,document.getElementById('root'));
 
