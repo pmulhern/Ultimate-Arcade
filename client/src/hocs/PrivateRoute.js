186cc046f0d2b59ca10131f70956720c0c2e87f0
Kevin Millhouse <Millhousekevin@gmail.com>
2020-10-25 11:07:13 -0400

    fork (#13)
    * fork (#1)
    
    * adding engine
    
    * proxy
    
    * added npm
    
    * build
    
    * deployment
    
    * deployment
    
    * f (#2)
    
    * adding engine
    
    * proxy
    
    * added npm
    
    * build
    
    * deployment
    
    * deployment
    
    * deployment
    
    * deploy
    
    * deployment
    
    * deployment

--- client/src/hocs/PrivateRoute.js
+++ client/src/hocs/PrivateRoute.js
@@ -1,7 +1,7 @@
 // Restricting the user from visiting the /login and /register routes as they are already logged into the system
 import React, { useContext } from "react";
 import { Route, Redirect } from "react-router-dom";
-import { AuthContext } from "../Context/AuthContext";
+import { AuthContext } from "../context/AuthContext";
 
 // First condition below is checking if user is authenticated
 const PrivateRoute = ({ component: Component, roles, ...rest }) => {
