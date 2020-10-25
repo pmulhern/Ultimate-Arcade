0100d8c97ea3f638993997c5a51fc20947220167
Kevin Millhouse <Millhousekevin@gmail.com>
2020-10-25 11:51:48 -0400

    forked (#16)
    * fork (#1)
    
    * adding engine
    
    * proxy
    
    * added npm
    
    * build
    
    * deployment
    
    * deployment
    
    * f (#2)
    
    * adding engine
    
    * proxy
    
    * added npm
    
    * build
    
    * deployment
    
    * deployment
    
    * deployment
    
    * deploy
    
    * deployment
    
    * deployment
    
    * deployment
    
    * deploy
    
    * deployment
    
    * debugging
    
    * debuggin
    
    * deploymeny
    
    * debugging
    
    * debugging

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,8 @@ import Home from "./components/Home";
 import Register from "./components/Register";
 import Admin from "./components/Admin";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import PrivateRoute from "./Hocs/PrivateRoute";
-import UnPrivateRoute from "./Hocs/UnPrivateRoute";
+import PrivateRoute from "./hocs/PrivateRoute";
+import UnPrivateRoute from "./hocs/UnPrivateRoute";
 import { Game } from "./Game/Game";
 
 function App() {
@@ -18,7 +18,7 @@ function App() {
         <UnPrivateRoute path="/register" component={Register} />
         <PrivateRoute path="/admin" roles={["admin"]} component={Admin} />
         <PrivateRoute path="/home" roles={["user", "admin"]} component={Home} />
-        <Route exact path="/game" component={Game} />
+        <Route exact path="/Game" component={Game} />
       </Switch>
     </Router>
   );
